f7ec1b6c13c91cce79efbb8493fced96cb9078ce
Apsara123127 <apsara.aitrich@gmail.com>
2025-05-05 10:25:11 +0530

    added

---
+++ OOPs/Activity/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using Inheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dog myDog = new Dog();
+
+        // Call method from parent class
+        myDog.MakeSound();
+
+        // Call method from child class
+        myDog.Bark();
+
+    }
+}
