b242c68425114058735492095a632f2b37123416
Apsara123127 <apsara.aitrich@gmail.com>
2025-05-28 11:40:03 +0530

    added

---
+++ Collections/Exception Handling/ApplicationException/ApplicationException/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+  
+            try
+            {
+                int age = 15;
+
+                if (age < 18)
+                {
+                    // Throwing a custom application exception
+                    throw new ApplicationException("Age must be 18 or above to register.");
+                }
+
+                Console.WriteLine("Registration successful.");
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine("Application Error: " + ex.Message);
+            }
+        }
+    }
