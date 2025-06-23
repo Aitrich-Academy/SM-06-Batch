d2bcaf1156b64ff4d86e5da18a55e852391ccb47
nishajoe <nishapauljoe@gmail.com>
2025-06-23 11:11:33 +0530

    added

---
+++ OOPs/Nisha_OOPS/OOPS_Workshop/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using Inheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Cat c = new Cat("Fluffy", 3, "Persian", true);
+       Dog d = new Dog("Buddy", 5, "Labrador", "Ball");
+
+        Console.WriteLine("----------Dog Details-----------");
+        d.DisplayDog();
+        Console.WriteLine("----------Cat Details-----------");
+        c.DisplayCat();
+    }
+}
