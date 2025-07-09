838c157c8ed9a3198b9da286679d4bb7c2159826
Apsara123127 <apsara.aitrich@gmail.com>
2025-07-09 12:42:27 +0530

    added

---
+++ EF/FK_EF/FK_EF/Program.cs
@@ -0,0 +1,22 @@
+﻿using FK_EF.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (var context = new AppDbContext())
+        {
+            // Add a Department
+            var dept = new Department { Name = "chinees" };
+            context.Departments.Add(dept);
+            context.SaveChanges();
+
+            // Add a Student linked to the above department
+            var student = new Student { FullName = "Navi", DepartmentId = dept.DepartmentId , DOB = new DateTime(2000, 5, 20) };
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully with Department");
+        }
+    }
+}
