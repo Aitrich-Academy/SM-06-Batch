3487c42753c531057e2d68520ed891ae25bb1574
Nimisha-AM <nimishaam757@gmail.com>
2025-07-26 12:52:55 +0530

    added

--- MVC/Crud_Manual1/Crud_Manual1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Crud_Manual1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
